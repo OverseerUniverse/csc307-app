ddb4b0a6a8dcfe1657dddb068fdd532892d5c29f
overseer <108105465+OverseerUniverse@users.noreply.github.com>
2026-04-06 13:42:31 -0700

    Overcomplicate the backend with typescript

---
+++ packages/express-backend/src/backend.ts
@@ -0,0 +1,49 @@
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+const port = 8000;
+
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req: Request, res: Response) => {
+  res.send(users);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
